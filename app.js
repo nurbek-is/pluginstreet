974e412fd861992e6c7d7cd0e086d1651d81b800
Nurbek Ismailov <nurbek82@gmail.com>
2016-09-02 13:28:37 -0700

    sep-d into 2 methods,created method to call other methods

--- app.js
+++ app.js
@@ -6,21 +6,21 @@ function station (city, building, fullAddress) {
   this.fullAddress = fullAddress;
   locations.push(this);
 }
-// Instantiating a new objects
+// Instantiating new objects
 var unionSqSea = new station ('seattle', 'Union Square', "601 Union St, Seattle, WA 98101");
 var pacificPlSea = new station ('seattle', 'Pacific Place', "705 Olive Way, Seattle, WA 98101");
 var SheratonTac = new station ('tacoma', 'City Center', "234 Main St, Tacoma, WA 98109");
 var BellevueMall = new station ('bellevue', 'Lincoln Square', '600 100th Pl NE, Bellevue, WA 98004');
 var concTechBell = new station ('bellevue', 'Concur Technologies', '601 108th Ave NE, Bellevue, WA 98004');
 var southParkPor = new station ('portland', 'South Park Seafood', '914 SW Taylor St. Portland, OR 97204');
-var hotelJupiPor = new station ('portland', 'Hotel Jupiter','800 East Burnside, Portland, Or 97214');
+var hotelJupiPor = new station ('portland', 'Hotel Jupiter','800 East Burnside, Portland, OR 97214');
 //Object literal
 var tracker = {
   getForm: document.getElementById('search'),
   searchWord: null,
   searchMatches: [],
 
-  getQueryDataPrint: function (event) {
+  getQueryData: function (event) {
     event.preventDefault();
     this.searchWord = event.target.searchName.value;
     var queryWord = this.searchWord.toLowerCase();
@@ -30,8 +30,15 @@ var tracker = {
       if (locations[i].city === queryWord) {
         console.log (locations[i].building + ", " +locations[i].fullAddress);
         tracker.searchMatches.push(locations[i].city)
+      }
+    }
+  },
+
+  displaySearchResults: function (event) {
+          event.preventDefault();
         if (tracker.searchMatches.length > 1) {
           console.log ('moreThan1')
+            console.log (tracker.searchMatches.length);
           var full_list = "";
           for (var i = 0; i < tracker.searchMatches.length; i++){
             full_list = full_list + locations[i].building + ", " + locations[i].fullAddress + '<br>'
@@ -39,16 +46,19 @@ var tracker = {
             var head1 = document.createElement('h1');
             head1.innerHTML = full_list;
             list.appendChild(head1);
+            console.log (tracker.searchMatches.length);
           }
         }
         else {
           var list = document.getElementById('image');
           var head1 = document.createElement('h1');
-          head1.innerHTML = locations[i].building + ", " +locations[i].fullAddress;
+          head1.innerHTML = "That city is not in our system yet"
           list.appendChild(head1);
-        }
-      }
     }
   },
+  runAllMethods: function () {
+    tracker.getQueryData (event);
+    tracker.displaySearchResults (event);
+  },
 }
-tracker.getForm.addEventListener('submit',tracker.getQueryDataPrint);
+tracker.getForm.addEventListener('submit',tracker.runAllMethods);
