40627ddbdea0a64d9d371acaa75bac7ebbcc23d8
Nurbek Ismailov <nurbek82@gmail.com>
2016-09-11 11:35:10 -0700

    added type of charger to constructor

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 var locations = [];
 // constructor function
-function station (city, building, fullAddress) {
+function station (city, chargeType, building, fullAddress) {
   this.city = city;
+  this.chargeType = chargeType;
   this.building = building;
   this.fullAddress = fullAddress;
   locations.push(this);
 }
 // Instantiating new objects
-var unionSqSea = new station ('seattle', 'Union Square', "601 Union St, Seattle, WA 98101");
-var pacificPlSea = new station ('seattle', 'Pacific Place', "705 Olive Way, Seattle, WA 98101");
-var SheratonTac = new station ('tacoma', 'City Center', "234 Main St, Tacoma, WA 98109");
-var BellevueMall = new station ('bellevue', 'Lincoln Square', '600 100th Pl NE, Bellevue, WA 98004');
-var concTechBell = new station ('bellevue', 'Concur Technologies', '601 108th Ave NE, Bellevue, WA 98004');
-var southParkPor = new station ('portland', 'South Park Seafood', '914 SW Taylor St. Portland, OR 97204');
-var hotelJupiPor = new station ('portland', 'Hotel Jupiter','800 East Burnside, Portland, OR 97214');
+var unionSqSea = new station ('seattle', 'Level 2', 'Union Square', "601 Union St, Seattle, WA 98101");
+var pacificPlSea = new station ('seattle','Level 2', 'Pacific Place', "705 Olive Way, Seattle, WA 98101");
+var SheratonTac = new station ('tacoma', 'DC Fast Charger', 'City Center', "234 Main St, Tacoma, WA 98109");
+var BellevueMall = new station ('bellevue', 'Level 2', 'Lincoln Square', '600 100th Pl NE, Bellevue, WA 98004');
+var concTechBell = new station ('bellevue', 'DC Fast Charger', 'Concur Technologies', '601 108th Ave NE, Bellevue, WA 98004');
+var southParkPor = new station ('portland', 'Level 2', 'South Park Seafood', '914 SW Taylor St. Portland, OR 97204');
+var hotelJupiPor = new station ('portland', 'Tesla Supercharger', 'Hotel Jupiter','800 East Burnside, Portland, OR 97214');
 //Object literal
 var tracker = {
   getForm: document.getElementById('search'),
