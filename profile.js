20dbbd0ce0a95153dd33824109feda8fdc12177e
Nurbek Ismailov <nurbek82@gmail.com>
2016-11-07 13:53:42 -0800

    added buidling,zip,label

--- profile.js
+++ profile.js
@@ -1,20 +1,28 @@
 var TheDatabase = firebase.database();
 function addStation() {
+    var building = document.getElementById('building').value;
     var address = document.getElementById('address').value;
     var city = document.getElementById('city').value;
     var state = document.getElementById('state').value;
+    var zip = document.getElementById('zip').value;
 
     var newStationEntry = TheDatabase.ref().child('stations').push().key;
+    TheDatabase.ref('stations/'+newStationEntry+'/building').set(building);
     TheDatabase.ref('stations/'+newStationEntry+'/address').set(address);
     TheDatabase.ref('stations/'+newStationEntry+'/city').set(city);
     TheDatabase.ref('stations/'+newStationEntry+'/state').set(state);
+    TheDatabase.ref('stations/'+newStationEntry+'/zip').set(zip);
 }
 
 var stationRef = TheDatabase.ref('stations');
 stationRef.on ('child_added', function (snapshot){
     snapshot.forEach(function(childSnapshot){
       var childData = childSnapshot.val();
-        alert(childData);
+    //  var name = snapshot.child("address").val();
+     var idElement = document.getElementById('addedAddress');
+     var liEL = document.createElement('li');
+     liEL.textContent = childData;
+     idElement.appendChild(liEL);
     });
 });
 
