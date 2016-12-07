2dac7a885ba4dd617c99cbbab0d9ee78a1365273
Nurbek Ismailov <nurbek82@gmail.com>
2016-12-07 13:51:50 -0800

    styling myev page

--- myev.js
+++ myev.js
@@ -18,77 +18,79 @@ var storage = firebase.storage().ref();
 var uploader = document.getElementById('uploader');
 var fileButton = document.getElementById ('fileButton');
 
+
 fileButton.addEventListener('change', function (e){
   //get file
   var file = e.target.files[0];
 // create a storage ref
-var storageRef = firebase.storage().ref('sweet_gifs/' + file.name);
+  var storageRef = firebase.storage().ref('AddedStations/' + file.name);
 
 //Upload file
-var task = storageRef.put(file);
+  var task = storageRef.put(file);
 
-// Update progress bar
-task.on('state_changed',
 
+//Update progress bar
+
+  task.on('state_changed',
 
     function progress (snapshot) {
-    var percentage = snapshot.bytesTransferred /
-    snapshot.totalBytes) * 100;
-    uploader.value = percentage;
-
-},
+      var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      uploader.value = percentage;
+    },
 function error (err) {
+  alert("Error: ")
+  alert(err);
 
 },
 function complete () {
+  //uploader.value = 100;
+}
+);
 
-},
-
-),
 });
 
 // Define function to create user model class
-function User(name, email) {
-  this.name = name;
-  this.email = email;
-  this.tesla = tesla.checked;
-  this.leaf = leaf.checked;
-  this.bmw = bmw.checked;
-};
-
-function getUserData(event) {
-  // Avoid page reloading on form submission
-  event.preventDefault();
-  // Get values from the form
-  var fullName = event.target.fullName.value;
-  var email = event.target.fullName.email;
-  // Create new instance of user model
-  var newUser = new User(fullName, email);
-  // Set data to local storage
-  localStorage.setItem('userProfile', JSON.stringify(newUser));
-}
-var personInfo = document.getElementById('personinfo')
-personInfo.addEventListener('submit', getUserData);
-
-// This function will store your name and your selection of car types in localStorage and display on right top corner
-function getUserName () {
-  if (localStorage.userProfile) {
-    console.log (localStorage.userProfile)
-    var getUserNameData = JSON.parse(localStorage.getItem('userProfile'));
-    console.log (getUserNameData);
-    console.log (getUserNameData.name);
-    var firstLetterName = getUserNameData.name.charAt(0).toUpperCase ();
-    var restLetterName = getUserNameData.name.slice(1);
-    document.getElementById('username').innerHTML = 'Hi ' + firstLetterName + restLetterName + "!";
-    if (getUserNameData.tesla === true) {
-      document.getElementById('icons').innerHTML = '<a href="https://www.tesla.com/" target = "_blank"><img src="images/tesla.png"/></a>'
-    }
-    if (getUserNameData.leaf === true) {
-      document.getElementById('icons2').innerHTML = '<a href="http://www.nissanusa.com/leaf" target = "_blank"><img src="images/nissan.jpg"/></a>'
-    }
-    if (getUserNameData.bmw === true) {
-      document.getElementById('icons3').innerHTML = '<a href="https://www.bmwusa.com/i3‎" target = "_blank"><img src="images/bmw.jpg"/></a>'
-    }
-  }
-};
-getUserName ();
+// function User(name, email) {
+//   this.name = name;
+//   this.email = email;
+//   this.tesla = tesla.checked;
+//   this.leaf = leaf.checked;
+//   this.bmw = bmw.checked;
+// };
+//
+// function getUserData(event) {
+//   // Avoid page reloading on form submission
+//   event.preventDefault();
+//   // Get values from the form
+//   var fullName = event.target.fullName.value;
+//   var email = event.target.fullName.email;
+//   // Create new instance of user model
+//   var newUser = new User(fullName, email);
+//   // Set data to local storage
+//   localStorage.setItem('userProfile', JSON.stringify(newUser));
+// }
+// var personInfo = document.getElementById('personinfo')
+// personInfo.addEventListener('submit', getUserData);
+//
+// // This function will store your name and your selection of car types in localStorage and display on right top corner
+// function getUserName () {
+//   if (localStorage.userProfile) {
+//     console.log (localStorage.userProfile)
+//     var getUserNameData = JSON.parse(localStorage.getItem('userProfile'));
+//     console.log (getUserNameData);
+//     console.log (getUserNameData.name);
+//     var firstLetterName = getUserNameData.name.charAt(0).toUpperCase ();
+//     var restLetterName = getUserNameData.name.slice(1);
+//     document.getElementById('username').innerHTML = 'Hi ' + firstLetterName + restLetterName + "!";
+//     if (getUserNameData.tesla === true) {
+//       document.getElementById('icons').innerHTML = '<a href="https://www.tesla.com/" target = "_blank"><img src="images/tesla.png"/></a>'
+//     }
+//     if (getUserNameData.leaf === true) {
+//       document.getElementById('icons2').innerHTML = '<a href="http://www.nissanusa.com/leaf" target = "_blank"><img src="images/nissan.jpg"/></a>'
+//     }
+//     if (getUserNameData.bmw === true) {
+//       document.getElementById('icons3').innerHTML = '<a href="https://www.bmwusa.com/i3‎" target = "_blank"><img src="images/bmw.jpg"/></a>'
+//     }
+//   }
+// };
+// getUserName ();
