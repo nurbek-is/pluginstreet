77b2bcc554498f361fd8f0962b3b045019df9c3e
Nurbek Ismailov <nurbek82@gmail.com>
2016-12-17 19:42:48 -0800

    fixed geacode no success

--- gmap.js
+++ gmap.js
@@ -1,4 +1,8 @@
 
+
+var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var labelIndex = 0;
+
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
@@ -8,26 +12,12 @@ function initMap() {
   var geocoder = new google.maps.Geocoder();
   geocodeSeveralAdresses(geocoder, map);
 
-  document.getElementById('submit').addEventListener('click', function() {
-    geocodeSearchButton(geocoder, map);
-  });
+  // document.getElementById('submit').addEventListener('click', function() {
+  //   geocodeSearchButton(geocoder, map);
+  // });
 }
 
-//// THis is General Search Function in Map, IT DOESN'T FIND ANY STATIONS//////
-function geocodeSearchButton(geocoder, resultsMap) {
-  var address = document.getElementById('address').value;
-  geocoder.geocode({'address': address}, function(results, status) {
-    if (status === 'OK') {
-      resultsMap.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker  ({
-          map: resultsMap,
-          position: results[0].geometry.location
-        });
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
-}
+
 
 /// this function PINS STATIONS ON THE MAP ////
 
@@ -37,15 +27,40 @@ function geocodeSeveralAdresses(geocoder, resultsMap) {
   for (var i = 0; i < addressesForCity.length; i++) {
     geocoder.geocode({'address': addressesForCity[i]}, function(results, status) {
       if (status === 'OK') {
+        console.log('label index is ' + labelIndex);
+        var popUpWindow = new google.maps.InfoWindow({
+          content: addressesForCity[labelIndex]
+        })
         resultsMap.setCenter(results[0].geometry.location);
         var marker = new google.maps.Marker  ({
+          label: labels[labelIndex++ % labels.length],
           map: resultsMap,
+          // title: addressesForCity[i];
           position: results[0].geometry.location
+        });
+        marker.addListener('click', function () {
+          popUpWindow.open(map, marker);
         });
       } else {
         alert('Geocode was not successful for the following reason: ' + status);
       }
     });
   }
-  addressesForCity = [];
 }
+
+
+//// THis is General Search Function in Map, IT DOESN'T FIND ANY STATIONS//////
+// function geocodeSearchButton(geocoder, resultsMap) {
+//   var address = document.getElementById('address').value;
+//   geocoder.geocode({'address': address}, function(results, status) {
+//     if (status === 'OK') {
+//       resultsMap.setCenter(results[0].geometry.location);
+//         var marker = new google.maps.Marker  ({
+//           map: resultsMap,
+//           position: results[0].geometry.location
+//         });
+//     } else {
+//       alert('Geocode was not successful for the following reason: ' + status);
+//     }
+//   });
+// }
