58c07ac588262f4fe80f902d2930a2d28ba9d2b8
Nurbek Ismailov <nurbek82@gmail.com>
2017-01-03 14:09:54 -0800

    google map workin w/ local storage only

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 
-var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-// var labelIndex = 0;
 
 // Object literal
 var tracker = {
@@ -10,6 +8,7 @@ var tracker = {
   matchFound: false,
   clearText: document.getElementById('displayArea'),
   matchedAddresses: [],
+  // addressString: [],
 
   getQueryDataNmatch: function (event) {
     event.preventDefault();
@@ -20,8 +19,12 @@ var tracker = {
     for (var i = 0; i < locations.length; i++) {
       if (locations[i].city === this.searchWord) {
         console.log (locations[i].chargeType + "," + locations[i].building + "," + locations[i].fullAddress);
-        tracker.matchedOneAdrs = locations[i].fullAddress;
         tracker.matchedAddresses.push(locations[i].fullAddress);
+        console.log (tracker.matchedAddresses);
+        // addressString = tracker.matchedAddresses.toString()
+        // console.log (addressString);
+        // tracker.matchedAddresses.push(locations[i].chargeType + "," + locations[i].building + "," + locations[i].fullAddress);
+        // console.log (tracker.matchedAddresses)
       };
     };
   },
@@ -31,23 +34,7 @@ var tracker = {
     for (var i = 0; i < tracker.WHATEV.length; i++) {
       buildingAddress = tracker.WHATEV[i];
       console.log('iteration :' + [i] + buildingAddress);
-      // var table = document.getElementById('displayArea');
-      // var tableRow = document.createElement('tr');
-      // var aTag = document.createElement('a');
-      // var tableData = document.createElement('td');
-      // aTag.href = 'index.html';
-      // aTag.innerHTML = buildingAddress;
-      // tableData.appendChild(aTag);
-      // tableRow.appendChild (tableData);
-      //
-      // var aTag4Charger = document.createElement('a');
-      // var tD = document.createElement('td');
-      // aTag4Charger.href = 'type.html';
-      // aTag4Charger.innerHTML = locations[i].chargeType;
-      // tD.appendChild(aTag4Charger);
-      // tableRow.appendChild (tD);
-      // table.appendChild(tableRow);
-      // tracker.matchFound = true;
+
     };
     if (tracker.matchFound === false) {
       console.log ('not found');
@@ -59,19 +46,14 @@ var tracker = {
   },
   addToLocalStorage: function () {
       localStorage.setItem('foundAddresses', JSON.stringify(tracker.matchedAddresses));
-    alert ('addresses are ' + localStorage.foundAddresses);
+      console.log (' local storage addresses are ' + localStorage.foundAddresses);
   },
-  // clearData: function() {
-  //   var clearText = document.getElementById('displayArea');
-  //   clearText.innerHTML = '';
-  //   tracker.WHATEV = [];
-  // },
+
   runAllMethods: function () {
-    //  tracker.clearData ();
 
      tracker.getQueryDataNmatch (event);
-    // tracker.displaySearchResults ();
-    tracker.addToLocalStorage ();
+    //  tracker.displaySearchResults ();
+     tracker.addToLocalStorage ();
     initMap();
     tracker.matchedAddresses = [];
 
@@ -83,27 +65,29 @@ var tracker = {
 
 
 
-
-
-
 /// this function PINS STATIONS ON THE MAP ////
 
 function geocodeSeveralAdresses(geocoder, resultsMap) {
+  var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   var labelIndex = 0;
+  // var addressString = 0;
  var addressesForCity = JSON.parse(localStorage.getItem('foundAddresses'));
-console.log ('addressesForCity is ' +  addressesForCity);
-  for (var i = 0; i < addressesForCity.length; i++) {
-    geocoder.geocode({'address': addressesForCity[i]}, function(results, status) {
+ console.log ('addressesForCity is ' +  addressesForCity);
+  for (var i = 0; i < tracker.matchedAddresses.length; i++) {
+    geocoder.geocode({'address': tracker.matchedAddresses[i]}, function(results, status) {
       if (status === 'OK') {
         console.log('label index is ' + labelIndex);
         var popUpWindow = new google.maps.InfoWindow({
-          content: addressesForCity[labelIndex]
+        content: addressesForCity [labelIndex]
+        // content: tracker.matchedAddresses[labelIndex]
+        // content: tracker.matchedAddresses[i]
+        // content: addressString[labelIndex]
+
         })
         resultsMap.setCenter(results[0].geometry.location);
         var marker = new google.maps.Marker  ({
           label: labels[labelIndex++ % labels.length],
           map: resultsMap,
-          // title: tracker.WHATEV[i];
           position: results[0].geometry.location
         });
         marker.addListener('click', function () {
